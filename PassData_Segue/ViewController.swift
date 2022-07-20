1af0ffc5d6a9a53b8e51f228d70512aaf514519c
Ngo Xuan Hieu <92346485+xhiew@users.noreply.github.com>
2022-07-20 14:10:29 +0700

    Initial Commit

---
+++ PassData_Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassData_Segue
+//
+//  Created by ADMIN on 20/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
